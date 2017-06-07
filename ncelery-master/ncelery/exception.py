517c72abcea8a5215a0473bcb957192e1762a2c7
cxx <chuxiaoxi@intra.legendsec.com>
2017-06-07 15:45:35 +0800

    init

---
+++ ncelery-master/ncelery/exception.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# -----------------------------------------------------
+#  FileName:    exception.py
+#  Author  :    wanghui@
+#  Project :    PA
+#  Date    :    2014-02-25 15:50
+#  Descrip :    
+# -----------------------------------------------------
+class RuntimeException(Exception):
+    pass
+
+class WooYunResultException(Exception):
+    pass
